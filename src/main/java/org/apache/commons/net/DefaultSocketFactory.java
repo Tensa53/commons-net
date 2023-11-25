7b9cc39ada5f810a9d1b8571c724c9ae47086060
FraPCA <francesco3b@gmail.com>
2023-11-25 16:50:52 +0100

    Fixed: 8 High Severity Bugs related to resources not being closed

--- src/main/java/org/apache/commons/net/DefaultSocketFactory.java
+++ src/main/java/org/apache/commons/net/DefaultSocketFactory.java
@@ -24,6 +24,7 @@ import java.net.Proxy;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.channels.IllegalBlockingModeException;
 
 import javax.net.SocketFactory;
 
@@ -122,9 +123,13 @@ public class DefaultSocketFactory extends SocketFactory {
     @Override
     public Socket createSocket(final InetAddress address, final int port) throws IOException {
         if (connProxy != null) {
-            final Socket s = new Socket(connProxy);
-            s.connect(new InetSocketAddress(address, port));
-            return s;
+            try(final Socket s = new Socket(connProxy))
+            {
+             s.connect(new InetSocketAddress(address, port));
+             return s;
+            } catch(final Exception e){
+                throw new IOException("Error during createSocket");
+            }
         }
         return new Socket(address, port);
     }
@@ -142,10 +147,14 @@ public class DefaultSocketFactory extends SocketFactory {
     @Override
     public Socket createSocket(final InetAddress address, final int port, final InetAddress localAddr, final int localPort) throws IOException {
         if (connProxy != null) {
-            final Socket s = new Socket(connProxy);
-            s.bind(new InetSocketAddress(localAddr, localPort));
-            s.connect(new InetSocketAddress(address, port));
-            return s;
+            try(final Socket s = new Socket(connProxy))
+            {
+                s.bind(new InetSocketAddress(localAddr, localPort));
+                s.connect(new InetSocketAddress(address, port));
+                return s;
+            }  catch(final Exception e){
+                throw new IOException("Error during createSocket");
+            }
         }
         return new Socket(address, port, localAddr, localPort);
     }
@@ -162,9 +171,13 @@ public class DefaultSocketFactory extends SocketFactory {
     @Override
     public Socket createSocket(final String host, final int port) throws UnknownHostException, IOException {
         if (connProxy != null) {
-            final Socket s = new Socket(connProxy);
-            s.connect(new InetSocketAddress(host, port));
-            return s;
+            try(final Socket s = new Socket(connProxy))
+            {
+                s.connect(new InetSocketAddress(host, port));
+                return s;
+            }  catch(final Exception e){
+                throw new IOException("Error during createSocket");
+            }
         }
         return new Socket(host, port);
     }
@@ -183,10 +196,15 @@ public class DefaultSocketFactory extends SocketFactory {
     @Override
     public Socket createSocket(final String host, final int port, final InetAddress localAddr, final int localPort) throws UnknownHostException, IOException {
         if (connProxy != null) {
-            final Socket s = new Socket(connProxy);
-            s.bind(new InetSocketAddress(localAddr, localPort));
-            s.connect(new InetSocketAddress(host, port));
-            return s;
+            try(final Socket s = new Socket(connProxy))
+            {
+                s.bind(new InetSocketAddress(localAddr, localPort));
+                s.connect(new InetSocketAddress(host, port));
+                return s;
+            } catch(final Exception e)
+            {
+              throw new IOException("Error during createSocket");
+            }
         }
         return new Socket(host, port, localAddr, localPort);
     }
